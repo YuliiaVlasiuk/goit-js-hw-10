92af7c3533c12da42c8accc8b77c8e0da09c6210
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-01-14 11:49:33 +0200

    4

--- src/index.js
+++ src/index.js
@@ -21,10 +21,11 @@ console.log(data.length);
   });
 }
 
-async function fetchCountries(name) {
-  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
     if (!response.ok) {
-        throw new Error(response.status);
+      throw new Error(response.status);
     }
-    return await response.json();
+    return response.json();
+  });
 }
