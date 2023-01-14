495f37add61977a7c461859b0751dfeb7fe204bf
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-01-14 12:45:24 +0200

    6

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
 let name = 'Ukraine';
 
-
 let inputSearch = document.querySelector('#search-box');
+let countryList=document.querySelector('.country-list');
 
 inputSearch.addEventListener('input', onSearching);
 
 function onSearching(evt) {
   name = evt.currentTarget.value;
-
+  countryList.innerHTML='';
   fetchCountries(name).then(data => {
-    if (data.length > 4) {
-        Notiflix.Notify.failure('Too many matches found. Please enter a more specific name.' );
-      return
-    }
-console.log(data.length);
-  });
+
+    if (data.length > 10) {
+      Notiflix.Notify.failure(
+        'Too many matches found. Please enter a more specific name.'
+      );
+      return;
+    } else 
+    {
+       if (data.length >=2 && data.length <=10){
+               
+      console.log(data.length);
+
+      countryList.innerHTML=createMarkup(data)}
+  }
+
+ console.log('one');
+
+})
 }
 
 function fetchCountries(name) {
@@ -29,3 +41,28 @@ function fetchCountries(name) {
     return response.json();
   });
 }
+
+function createMarkup(arr) {
+  console.log(arr);
+  return arr
+    .map(
+      ({
+        name: {official:name},
+        flags: {svg :flag},
+      }) =>
+        ` <li  class="list_of_countries">
+          <img class="flag" src="${flag}" alt="${name}" />
+          <h3>${name}</h3>
+         </li>`
+    )
+    .join('');
+}
+
+Notiflix.Notify.init({
+  position: 'center-top',
+  width: '300px',
+  distance: '10px',
+  opacity: 1,
+  rtl: false,
+  timeout: 1000,
+});
