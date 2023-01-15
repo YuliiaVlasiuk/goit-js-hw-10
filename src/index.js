a206e42982dfe58d588c06ec5b773c45b10e8a0d
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-01-15 14:19:22 +0200

    all

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './css/styles.css';
 import Notiflix from 'notiflix';
+import fetchCountries from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 var debounce = require('lodash.debounce');
@@ -8,31 +9,32 @@ let name = 'Ukraine';
 let inputSearch = document.querySelector('#search-box');
 let countryList = document.querySelector('.country-list');
 
-
-inputSearch.addEventListener('input', debounce(onSearching,300))
+inputSearch.addEventListener('input', debounce(onSearching, DEBOUNCE_DELAY));
 
 function onSearching(evt) {
   name = evt.target.value.trim();
   countryList.innerHTML = '';
 
-  if (!name){
-        return alert('add value')
-     }
-  
-fetchCountries(name).then(data => {
-    if (data.length > 10) {
-      Notiflix.Notify.failure(
-        'Too many matches found. Please enter a more specific name.'
-      );
-      return;
-    } else {
-      if (data.length > 1 && data.length <= 10) {
-          countryList.innerHTML = createMarkupList(data);
+  if (!name) {
+    return alert('add value');
+  }
+
+  fetchCountries(name)
+    .then(data => {
+      if (data.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
       } else {
-        countryList.innerHTML = createMarkup(data);
+        if (data.length > 1 && data.length <= 10) {
+          countryList.innerHTML = createMarkupList(data);
+        } else {
+          countryList.innerHTML = createMarkup(data);
+        }
       }
-    }
-  });
+    })
+    .catch(err => console.log(err));
 }
 
 function createMarkupList(arr) {
@@ -54,12 +56,13 @@ function createMarkup(arr) {
   console.log(arr);
   return arr
     .map(
-      ({ name: { official: name }, 
+      ({
+        name: { official: name },
         flags: { svg: flag },
         capital,
         population,
-        languages    
-    }) =>
+        languages,
+      }) =>
         ` <li>
                   <div class="list_of_countries" >
                     <img class="flag" src="${flag}" alt="${name}" />
@@ -67,22 +70,13 @@ function createMarkup(arr) {
                   </div>
                   <p> <b>Capital</b>: ${capital}</p>
                   <p> <b> Population: </b>${population}</p>
-                  <p> <b> Languages:</b> ${Object.values(languages).join(', ')}</p>
+                  <p> <b> Languages:</b> ${Object.values(languages).join(
+                    ', '
+                  )}</p>
             </li>`
-    ).join('');
+    )
+    .join('');
 }
-
-function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    });
-  }
-  
-  
-
 
 Notiflix.Notify.init({
   position: 'center-top',
